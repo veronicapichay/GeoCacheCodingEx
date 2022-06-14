753476d3fc8c828fa4123b1343282bec02411b5a
Veronica Pichay <veronicapichay@outlook.com>
2022-06-14 15:05:30 -0700

    Models

---
+++ GeoCacheCodingEx/Data/Models/GeoCache.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace GeoCacheCodingEx.Data.Models
+{
+    public class GeoCache
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+        
+        public virtual ICollection<GeoCacheItem> Items { get; set; }
+
+
+
+
+
+    }
+}
---
+++ GeoCacheCodingEx/Data/Models/GeoCacheItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GeoCacheCodingEx.Data.Models
+{
+    public class GeoCacheItem
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual GeoCache GeoCache { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? ActiveDates { get; set; }
+
+
+
+
+
+    }
+}
